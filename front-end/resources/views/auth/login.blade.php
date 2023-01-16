ca0a17adfe92281c25646f97cc0c2d75980f2a20
Michelle Mujica <mmujica@espol.edu.ec>
2023-01-15 23:17:17 -0500

    vista login

--- front-end/resources/views/auth/login.blade.php
+++ front-end/resources/views/auth/login.blade.php
@@ -2,19 +2,26 @@
 @section('title','Login') <!-- nombre del titulo de la pagina-->
 <link href="{{ asset('\css\login.css') }}" rel="stylesheet">
 @section('content')
-<div class="container shadow p-3 mb-5 mt-5 rounded" id="container-image">
-    <h1 class="text-center ">Iniciar Sesion</h1>
+<div class="container shadow p-5 mb-5 mt-5 rounded" id="container-image">
+    <div class="wrap">
+        <h1 class="text-center m-0">Iniciar Sesion</h1>
+    </div>
     <div class="abs-center">
-      <form action="#" class="border p-3 form">
+      <form action="#" class="m-0 form" method="">
         <div class="form-group">
           <label for="email">Email</label>
-          <input type="email" name="email" id="email" class="form-control">
+          <input type="email" name="email" id="email" class="form-control" >
         </div>
         <div class="form-group">
           <label for="password">Password</label>
           <input type="password" name="password" id="password" class="form-control">
         </div>
-        <button type="submit" class="btn btn-primary">Login</button>
+
+        {{-- <p class="border border-danger rounded-sm p-2 my-2 text-danger">* Error</p> --}}
+
+        <div class="text-center p-4" >
+            <button type="submit" class="btn" id="btn-login">Login</button>
+        </div>
       </form>
     </div>
   </div>
