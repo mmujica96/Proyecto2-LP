3a3a9a0a50c8205f6e99f06ae41b20a2edf359df
unknown <hempghans@gmail.com>
2023-02-01 00:01:38 -0500

    x

--- front-end/resources/views/user/perfilPsicologo.blade.php
+++ front-end/resources/views/user/perfilPsicologo.blade.php
@@ -7,25 +7,8 @@
 
     <title>User - Citas Psicologos</title> <!--title, estable el nombre del titulo de la pagina @yield('title') -->
     <link rel="stylesheet" href="{{ asset('css/perfilPsicologo.css') }}">
-<<<<<<< HEAD
-    
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <!-- Optional JavaScript -->
-   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-   <!-- Data Table -->
-    
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.22/r-2.2.6/sc-2.0.3/sb-1.0.0/sp-1.2.1/datatables.min.css"/>
- 
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.22/r-2.2.6/sc-2.0.3/sb-1.0.0/sp-1.2.1/datatables.min.js"></script>
-
-=======
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
->>>>>>> 19acc7c42f2fceaf9995f5a36edde8b15e671ff4
  <!-- Font awesome -->
    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
 
@@ -45,25 +28,12 @@
                   <div class="col h4 text-center pl-2">David Rendon</div>
               </div>
             </div>
-<<<<<<< HEAD
-            </div>
-            <div class="col-sm-2-4" style="text-align: right;"S>
-                <a class="nav-link text-white" href="login"><i class="fas fa-sign-out-alt"></i></a>
-            </div>
-        </div>
-     </nav>
-   </header>
-    <?
-        
-    ?>
-=======
               <div class="col-sm col-8" style="text-align: right;"S>
                   <a class="nav-link text-white" href="login"><i class="fas fa-sign-out-alt"></i></a>
               </div>
           </div>
        </nav>
        </header>
->>>>>>> 19acc7c42f2fceaf9995f5a36edde8b15e671ff4
 
    <main> 
     <?php
@@ -88,15 +58,10 @@
                 </div>
             </div>
             <div class="ubicacion">
-<<<<<<< HEAD
-                <div class="cuadroGris" data-toggle="modal" data-target="#exampleModal"> 
-                    <div class="more" href> <button id="modal-button" data-toggle="modal" data-target=".bd-example-modal-lg">+</button></div>
-=======
                 <div class="cuadroGris"> 
                     <div class="container text-center py-1">
                         <button class="btn btn-success btn-sm" id="modal-button" data-bs-toggle="modal" data-bs-target="#exampleModal">+</button> 
                     </div>
->>>>>>> 19acc7c42f2fceaf9995f5a36edde8b15e671ff4
                     <p>Agendar cita</p>
                 </div>
             </div>    
